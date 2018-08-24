7ca43d48d9a5cb2e055207be9d9d7ab7cd02d081
Jonas Almeida Cezar <jcezar.al@gmail.com>
2018-08-24 09:02:49 -0300

    Novas aulas

--- pkg/learn/http-client/main.go
+++ pkg/learn/http-client/main.go
@@ -1,29 +1,83 @@
 package main
 
 import (
-  "fmt"
-  "time"
-  "net"
-  "net/http"
-  "io/ioutil"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"time"
 )
+
+type blogPost struct {
+	UserID  int    `json:"userId"`
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"body"`
+}
 
 func main() {
 
-  var netTransport = &http.Transport{
-  Dial: (&net.Dialer{
-    Timeout: 5 * time.Second,
-  }).Dial,
-  TLSHandshakeTimeout: 5 * time.Second,
-  }
+	var httpTransport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
 
-  var netClient = &http.Client{
-    Timeout: time.Second * 10,
-    Transport: netTransport,
-  }
+	var httpClient = &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: httpTransport,
+	}
 
-  response, _ := netClient.Get("http://google.com")
-  body, _ := ioutil.ReadAll(response.Body)
+	httpGET(httpClient)
 
-  fmt.Println("finished request body=", string(body))
+	httpPOST(httpClient)
+}
+
+func httpGET(httpClient *http.Client) {
+	response, err := httpClient.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		fmt.Printf("Could not read response\n%v\n", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Could not read response body\n%v\n", err)
+		return
+	}
+
+	blogPosts := []blogPost{}
+	err = json.Unmarshal(body, &blogPosts)
+	if err != nil {
+		fmt.Printf("Could not parse 'blogPosts'\n%v\n", err)
+		return
+	}
+
+	for i, blogPost := range blogPosts {
+		fmt.Printf("BlogPost [%d] [%+v]\n", i, blogPost)
+	}
+
+}
+
+func httpPOST(httpClient *http.Client) {
+
+	blogPost := blogPost{UserID: 1, Title: "Golang http", Content: "Hello world!"}
+	postBody, err := json.Marshal(blogPost)
+	if err != nil {
+		fmt.Printf("Could not convert blogPost to json\n%v\n", err)
+	}
+
+	response, err := httpClient.Post("https://jsonplaceholder.typicode.com/posts", "application/json", bytes.NewBuffer(postBody))
+	if err != nil {
+		fmt.Printf("Could not read response\n%v\n", err)
+		return
+	}
+	defer response.Body.Close()
+
+	fmt.Printf("Status code returnuded: %v\n", response.StatusCode)
+
 }
